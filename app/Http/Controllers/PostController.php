32bac45e2a04248eddf3c492f0718e56876e798c
Ajay S <ajay_sieunarine@hotmail.com>
2022-04-03 21:11:36 -0400

    start adding CRUD functionality for Posts (list)
    - add endpoints in web.php
    
    - add controller methods in postcontroller.php
    
    - show how to use backend data in view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
+    public function list(){
+        $posts = Post::orderByDesc('created_at')->get();
+        return view('list', ['posts' => $posts]);
+    }
+    public function create(){
+
+    }
+    public function store(){
+
+    }
+    public function update(){
+
+    }
+    public function edit(){
+
+    }
+    public function destroy(){
+
+    }
 }
